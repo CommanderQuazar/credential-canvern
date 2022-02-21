07eb6151a5b5f66477641710bb4206e6cabd1724
Tobey Ragain <tobeyragain@gmail.com>
2022-02-20 23:03:04 -0800

    SQLConnection class finished (testing)

--- include/SQLConnector.h
+++ include/SQLConnector.h
@@ -5,12 +5,48 @@
 #ifndef CREDENTIAL_CANVERN_SQLCONNECTOR_H
 #define CREDENTIAL_CANVERN_SQLCONNECTOR_H
 
+#include <mysql/mysql.h>
+
+#include <iostream>
+
 
 class SQLConnector
 {
-public:
+    public:
+        SQLConnector(std::string *  host_name,
+                     std::string *  user_name,
+                     std::string *  password,
+                     std::string *  database,
+                     unsigned int   port_number,
+                     std::string *  unix_socket_number = nullptr,
+                     unsigned long  client_f = 0) :
+                     host           (host_name),
+                     user           (user_name),
+                     passwd         (password),
+                     db             (database),
+                     port           (port_number),
+                     unix_socket    (unix_socket_number),
+                     client_flag    (client_f)
+                     { };
 
-private:
+        SQLConnector&       connect();
+        inline std::string  stats() { return mysql_stat(&mysql); };
+        inline bool         status() { return mysql_ping(&mysql); };
+        SQLConnector&       disconnect();
+
+        ~SQLConnector() = default;
+
+    protected:
+        MYSQL mysql {};
+
+    private:
+        std::string *   host;
+        std::string *   user;
+        std::string *   passwd;
+        std::string *   db;
+        unsigned int    port;
+        std::string *   unix_socket;
+        unsigned long   client_flag;
 };
 
 
--- src/SQLConnector.cpp
+++ src/SQLConnector.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "../include/SQLConnector.h"
+
+SQLConnector& SQLConnector::connect()
+{
+    mysql_init(&mysql);
+    mysql_real_connect(&mysql,
+                       host->c_str(),
+                       user->c_str(),
+                       passwd->c_str(),
+                       db->c_str(),
+                       port,
+                       (unix_socket == nullptr ? nullptr : unix_socket->c_str()),
+                       client_flag);
+    return *this;
+}
+
+/*
+ * Closes the connection to MySQL database
+ * The deconstrcutor will typically follow a call to this function
+ */
+SQLConnector &SQLConnector::disconnect()
+{
+    mysql_close(&mysql);
+    return *this;
+}
+
+
+
