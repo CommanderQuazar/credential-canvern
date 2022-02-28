88269d0a592d0f8fbb415d93b8a195fbe1df3310
Tobey Ragain <tobeyragain@gmail.com>
2022-02-27 22:09:58 -0800

    refined the protected interface for UserAccount class

--- server/include/User.h
+++ server/include/User.h
@@ -10,31 +10,45 @@
 
 #include <utility>
 
+typedef struct Credentials
+{
+    std::string passphrase;
+    std::string password;
+};
+
 class User
 {
     public:
-        User()= delete;
+        User() = delete;
         User(SqlConnector * my_sql_connection, std::string username_entry,
              std::string passph_entry) :
             _connection(my_sql_connection),
-            _entry_user_n(std::move(username_entry)),
-            _entry_pass_ph(std::move(passph_entry))
+            _entry_usern(std::move(username_entry)),
+            _entry_passph(std::move(passph_entry))
             { };
 
         User& create();
-
-        //DEV NOTE: This must get a user table to operate on once logged in
         User& login();
+        unsigned int get_user_id() const { return _u_table_id; };
 
+        ~User() = default;
 
     protected:
+        User(std::string username_entry,
+             std::string passph_entry) :
+            _entry_usern(std::move(username_entry)),
+            _entry_passph(std::move(passph_entry))
+            { };
+        Credentials& mysql_get_credentials();
 
+        //Stores the table ID of the logged in user
+        unsigned int _u_table_id {0};
+        std::string _entry_usern;
+        std::string _entry_passph;
 
     private:
         SqlConnector * _connection;
-        std::string _entry_user_n;
-        std::string _entry_pass_ph;
-        unsigned int _u_id {};
+
 };
 
 
