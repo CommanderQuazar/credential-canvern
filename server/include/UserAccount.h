0e5602dcb5cda85d8c44b3c2de1e8ec0b0df610b
Tobey Ragain <tobeyragain@gmail.com>
2022-02-27 22:11:35 -0800

    created UserAccount interface

--- server/include/UserAccount.h
+++ server/include/UserAccount.h
@@ -5,11 +5,23 @@
 #ifndef CREDENTIAL_CAVERN_SERVER_INCLUDE_USERACCOUNT_H_
 #define CREDENTIAL_CAVERN_SERVER_INCLUDE_USERACCOUNT_H_
 
-class UserAccount
-{
-    public:
+#include "User.h"
 
-    private:
+class UserAccount : User
+{
+    using User::_u_table_id;
+    using User::_entry_usern;
+    using User::_entry_passph;
+
+    public:
+        UserAccount() = delete;
+        UserAccount(const std::string or_usern, const std::string or_passph) :
+            User(or_usern, or_passph)
+            { };
+
+        UserAccount& reset_umane(const std::string& new_usern);
+        UserAccount& reset_pass(const std::string& new_passph);
+        void remove();
 
 };
 
