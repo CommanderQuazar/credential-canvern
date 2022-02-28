f44f5976d8f0bfc126d7f6ffcb3bec0fae366b56
Tobey Ragain <tobeyragain@gmail.com>
2022-02-27 21:06:01 -0800

    UserAccount class added

---
+++ server/include/UserAccount.h
@@ -0,0 +1,16 @@
+//
+// Created by Tobey Ragain on 2/27/22.
+//
+
+#ifndef CREDENTIAL_CAVERN_SERVER_INCLUDE_USERACCOUNT_H_
+#define CREDENTIAL_CAVERN_SERVER_INCLUDE_USERACCOUNT_H_
+
+class UserAccount
+{
+    public:
+
+    private:
+
+};
+
+#endif //CREDENTIAL_CAVERN_SERVER_INCLUDE_USERACCOUNT_H_
---
+++ server/src/UserAccount.cc
@@ -0,0 +1,6 @@
+//
+// Created by Tobey Ragain on 2/27/22.
+//
+
+#include "../include/UserAccount.h"
+
