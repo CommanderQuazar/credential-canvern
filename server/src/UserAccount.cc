41480c0a37c684eaf43dd89c6715fe078bcca8cc
Tobey Ragain <tobeyragain@gmail.com>
2022-02-27 22:11:11 -0800

    skeleton member function definitions

--- server/src/UserAccount.cc
+++ server/src/UserAccount.cc
@@ -4,3 +4,31 @@
 
 #include "../include/UserAccount.h"
 
+/*
+ * If the entered user credentials are confirmed the new username
+ * will be updated in the user's account.
+ *
+ * If _u_table_id is equal to zero, it means the
+ */
+UserAccount &UserAccount::reset_umane(const std::string& new_passph)
+{
+    return <#initializer#>;
+}
+
+/*
+ * If the entered user credentials are confirmed the new password
+ * will be updated in the user's account.
+ */
+UserAccount &UserAccount::reset_pass(const std::string& new_passph)
+{
+    return <#initializer#>;
+}
+
+/*
+ * Torches all data in user's account and removes it from
+ * the user table.
+ */
+void UserAccount::remove()
+{
+
+}
