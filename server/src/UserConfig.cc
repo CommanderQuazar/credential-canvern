fd6ac4a9afbb31768a51993b4bb1a9f159879386
Tobey Ragain <tobeyragain@gmail.com>
2022-03-03 22:57:30 -0800

    config puller complete

--- server/src/UserConfig.cc
+++ server/src/UserConfig.cc
@@ -9,13 +9,23 @@
  */
 configurations_t UserConfig::pull_configs()
 {
+    std::string query ("SELECT * FROM UserConfig WHERE user_fk= '" + _user_id + "'");
+    if(mysql_query(_server->connection(), query.c_str()))
+    {
+        _server->log("SERVER ERROR: Could not find a valid config row");
+        return {._f_valid = false};
+    }
 
+    MYSQL_RES * mysqlResult = mysql_store_result(_server->connection());
+    MYSQL_ROW userConfigRow = mysql_fetch_row(mysqlResult);
+
+    return { userConfigRow[1], userConfigRow[2], userConfigRow[3], true};
 }
 
 /*
  * Pushes a new logout period to the database
  */
-UserConfig &UserConfig::push_logout_period(logout_period_e period)
+UserConfig &UserConfig::push_logout_period(std::string period)
 {
 
 }
@@ -23,7 +33,7 @@ UserConfig &UserConfig::push_logout_period(logout_period_e period)
 /*
  * Pushes a new GUI theme to the database
  */
-UserConfig &UserConfig::push_theme(theme_color_e theme_color)
+UserConfig &UserConfig::push_theme(std::string theme_color)
 {
 
 }
