6d05e89203bba4375a21ff228deb667a5ea4e4c1
Tobey Ragain <tobeyragain@gmail.com>
2022-04-01 18:25:13 -0700

    skeleton for implementation

--- server/src/SessionLog.cc
+++ server/src/SessionLog.cc
@@ -3,3 +3,29 @@
 //
 
 #include "../include/SessionLog.h"
+
+/*
+ * Pushes a new login entry to the mysql db
+ */
+unsigned int SessionLog::push_login()
+{
+    return 0;
+}
+
+/*
+ * Clears ALL entries from the table
+ */
+unsigned int SessionLog::clear_logins()
+{
+    return 0;
+}
+
+/*
+ * Returns a session_log_t that holds ALL login entries
+ * to the user's account (time) & (ip address)
+ */
+session_log_t SessionLog::pull_logins()
+{
+
+
+}
