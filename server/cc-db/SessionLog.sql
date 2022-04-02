342d14acc04b67e3bd86e1e415bc5350c5b20fc8
Tobey Ragain <tobeyragain@gmail.com>
2022-04-01 19:12:39 -0700

    updated sessionLog sql table

--- server/cc-db/SessionLog.sql
+++ server/cc-db/SessionLog.sql
@@ -2,6 +2,7 @@ create table SessionLog
 (
     session_log_id  int auto_increment
         primary key,
+    hostname        varchar(32)                     not null,
     logged_ip       varchar(32) default '127.0.0.1' not null,
     logged_datetime datetime                        not null,
     user_fk         int                             not null,
