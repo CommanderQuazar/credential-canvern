989569749a17e930f5a2c35608ed6e606d245d18
Tobey Ragain <tobeyragain@gmail.com>
2022-04-26 23:38:10 -0700

    fixed SessionLog puller

--- server/cc-db/SessionLog.sql
+++ server/cc-db/SessionLog.sql
@@ -5,11 +5,8 @@ create table SessionLog
     hostname        varchar(32)                     not null,
     logged_ip       varchar(32) default '127.0.0.1' not null,
     logged_datetime datetime                        not null,
-    user_fk         int                             not null,
+    user_fk         int                             null,
     constraint SessionLog_Users_user_id_fk
         foreign key (user_fk) references Users (user_id)
 );
-
-create index SessionLog_session_log_id_index
-    on SessionLog (session_log_id);
 
